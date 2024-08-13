65b35c09b5f349f6bcf59156d2cc06e0b30870a0
AbeamusBrandon <ldphilbin@gmail.com>
2024-08-12 21:24:20 -0400

    Initial commit Currently supported features:     Scene setup     Scene rendering         Diffuse reflections         Specular reflections     Writing images to files     Toggleable status updates (w/ timestamps)     Writing status updates to file once complete     2 types of meshes         Polygon mesh             Cube mesh (extends polygon mesh)         Sphere mesh     Writing images to disk during render and retrieving them at the end to avoid running out of memory I can't think of anything else right now, but there probably is some other stuff

---
+++ src/driver/Main.java
@@ -0,0 +1,36 @@
+package driver;
+
+import renderer.Image;
+import renderer.RenderSettings;
+import renderer.Renderer;
+import scene.Scene;
+import scene.scenes.TestScene3;
+import util.Debug;
+
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Debug.setPrintLogs(true);
+
+        Scene scene = new TestScene3();
+        Dimension imageSize = new Dimension(3024, 1964);
+        int recursionCount = 30;
+        int frameCount = 100;
+        int threadCount = 50;
+        int rngSeed = 0;
+
+        RenderSettings settings = new RenderSettings(scene);
+        //settings.setSize(imageSize);
+        settings.setRecursionCount(recursionCount);
+        settings.setFrameCount(frameCount);
+        settings.setThreadCount(threadCount);
+        settings.setRngSeed(rngSeed);
+        settings.setHoldingDir("holding_dir");
+
+        Image image = Renderer.render(settings);
+
+        image.writeToFile(settings.toFilenameString() + ".png");
+        Debug.writeLogsToFile(settings.toFilenameString() + ".txt");
+    }
+}
